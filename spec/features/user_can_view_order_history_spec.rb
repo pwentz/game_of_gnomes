02b369a4adb8ff6f040cef33a4c98b3c746cea93
Kris Sparks <kbs5280@gmail.com>
2016-07-28 14:36:13 -0600

    CSS form fix

---
+++ spec/features/user_can_view_order_history_spec.rb
@@ -0,0 +1,12 @@
+# require 'rails_helper'
+#
+# describe "User can view order history", type: :feature do
+#   scenario "when user visits /orders" do
+#     user = create(:user)
+#     orders = Order.create(user_id: user.id)
+#
+#     visit orders_path
+#
+#     expect(page).to have_content(order.id)
+#   end
+# end
