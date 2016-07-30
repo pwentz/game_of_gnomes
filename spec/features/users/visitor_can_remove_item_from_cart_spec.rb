a615f3ba256eb8c5e5e3d05832996220822a31d1
Kris Sparks <kbs5280@gmail.com>
2016-07-30 13:17:10 -0600

    Integrated new styling
    Fixed application page, all tests passing. Ready to add navigation and fix
    broken links. Focus on UX and additional styling.

--- spec/features/users/visitor_can_remove_item_from_cart_spec.rb
+++ spec/features/users/visitor_can_remove_item_from_cart_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
-# Background: My cart has an item in it
-# As a visitor
-# When I visit "/cart"
-# And I click link "Remove"
-# Then my current page should be "/cart"
-# And I should see a message styled in green
-# And the message should say "Successfully removed SOME_ITEM from your cart."
-# And the title "SOME_ITEM" should be a link to that item in case the visitorgc wants to add it back
-# And I should not see the item listed in cart
-
 describe "visitor can remove item from cart", :type => :feature do
   context "visitor has one of each item and removes 1" do
     scenario "and visitor sees cart with item removed" do
